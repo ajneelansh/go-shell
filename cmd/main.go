a012a094b1bf01ab8ea71d16c2f742038b7a43e4
ajneelansh <ajneelansh@gmail.com>
2025-08-21 22:24:18 +0530

    implemented echo

--- cmd/main.go
+++ cmd/main.go
@@ -3,28 +3,59 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
+	// "go-shell/pkg/built-ins"
+	"go-shell/pkg/built_ins"
 	"go-shell/pkg/input_parser"
+	"os"
 )
 
 func main(){
 
 	reader := bufio.NewReader(os.Stdin)
 
-  for {
-	  
-	  fmt.Printf("[my_shell]:~$")
-    
-	 input,err := reader.ReadString('\n')
 
-	 if err!=nil{
+  for {
+	initial_directory, err := os.Getwd()
+
+	if err!=nil {
+		fmt.Fprintln(os.Stderr, "Error getting current directory : ",err)
+	}
+
+	fmt.Printf("[my_shell]@~%s: $ ",initial_directory)
+    
+	input,err := reader.ReadString('\n')
+
+	if err!=nil{
 		fmt.Fprintln(os.Stderr, "Error reading input : ",err)
 		continue
+	}
+
+    tokens := input_parser.TokenizeInput(input)
+	
+	
+
+	
+
+   if(len(tokens)!=0) { 
+
+		switch tokens[0] {
+			case "cd": 
+	    		built_ins.Cd(tokens,initial_directory)
+		    case "echo":
+				built_ins.Echo(tokens)
+			case "which":
+				// built_ins.Which(tokens,initial_directory)
+			case "env":
+				// built_ins.Env(tokens,initial_directory)
+			case "pwd":
+				built_ins.Pwd()
+			case "exit":
+				return
+	  
 	 }
 
-	 fmt.Println(input);
-     tokens := input_parser.TokenizeInput(input)
-	 fmt.Println(tokens)
+
+   }
 
 
   }
