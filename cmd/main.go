06cf98d3b5ef4e1efa4ec8667a741cb5b33156da
ajneelansh <ajneelansh@gmail.com>
2025-08-25 01:59:21 +0530

    implemented pipes

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	// "go-shell/pkg/built-ins"
+	"os/exec"
+	"strings"
+
 	"go-shell/pkg/built_ins"
 	"go-shell/pkg/input_parser"
+	"go-shell/pkg/core"
+
 	"os"
 )
 
@@ -29,8 +33,13 @@ func main(){
 		fmt.Fprintln(os.Stderr, "Error reading input : ",err)
 		continue
 	}
-
-    tokens := input_parser.TokenizeInput(input)
+   
+	var tokens []string
+	if strings.Contains(input,"|"){
+		core.ExecutePipe(input)
+	} else {
+		tokens = input_parser.TokenizeInput(input)
+	}
 	
 	
 
@@ -44,14 +53,21 @@ func main(){
 		    case "echo":
 				built_ins.Echo(tokens)
 			case "which":
-				 built_ins.Which(tokens)
-			case "env":
-				// built_ins.Env(tokens,initial_directory)
+				built_ins.Which(tokens)
+			case "export":
+				built_ins.Export(tokens)
 			case "pwd":
 				built_ins.Pwd()
 			case "exit":
 				return
-	  
+			default:
+				cmd := exec.Command(tokens[0],tokens[1:]...)
+				cmd.Stdout = os.Stdout
+				cmd.Stdin = os.Stdin
+				cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				fmt.Fprintln(os.Stderr,"Command failed: ", err)
+			}
 	 }
    }
   }
